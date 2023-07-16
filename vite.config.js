ba34298418852946340aaa7597fd03d9201d90af
Saurabh Sen <saurabh.sen.cs19@ggits.net>
2023-07-16 13:00:21 +0530

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html'),
+      },
+    },
+  },
   plugins: [react()],
 })
